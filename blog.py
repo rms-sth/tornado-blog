fbbef7db302b969ec1f0d21e8016f2f23ac6ec65
Ramesh Pradhan <pyrameshpradhan@gmail.com>
2020-08-18 23:57:57 +0545

    Refactor: Change all handler to async + user async_peewee

--- blog.py
+++ blog.py
@@ -1,67 +1,71 @@
 import os
 
+from peewee_async import Manager
+
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 
-from tornado.web import Application
 from tornado.options import define, options
 from tornado.web import url
 
-from models import Blog
+from models import Blog, db
 
 define("port", default=8000, help="run on the given port", type=int)
 
 
-class Application(tornado.web.Application):
-    def __init__(self):
-        handlers = [
-            url(r"/", BlogList, name="blog_list"),
-            url(r"/blog-detail/([0-9Xx\-]+)/", BlogDetail, name="blog_detail"),
-            url(r"/blog-create/", BlogCreate, name="blog_create"),
-            url(r"/blog-edit/([0-9Xx\-]+)/", BlogCreate, name="blog_edit"),
-        ]
-        settings = dict(
-            debug=True,
-            template_path=os.path.join(os.path.dirname(__file__), "templates"),
-        )
-        tornado.web.Application.__init__(self, handlers, **settings)
-
-
 class BlogList(tornado.web.RequestHandler):
-    def get(self):
-        blogs = Blog.select()
+    async def get(self):
+        query = Blog.select().order_by(Blog.created_date.desc())
+        blogs = await self.application.objects.execute(query)
         self.render("blog_list.html", blogs=blogs)
 
 
 class BlogDetail(tornado.web.RequestHandler):
-    def get(self, id):
-        blog = Blog.select().where(Blog.id == id).get()
+    async def get(self, id):
+        blog = await self.application.objects.get(Blog, id=id)
         self.render("blog_detail.html", blog=blog)
 
 
 class BlogCreate(tornado.web.RequestHandler):
-    def get(self, id=None):
+    async def get(self, id=None):
         blog = dict()
         if id:
-            blog = Blog.select().where(Blog.id == id).get()
+            blog = await self.application.objects.get(Blog, id=id)
         self.render("blog_create.html", blog=blog)
 
-    def post(self, id=None):
+    async def post(self, id=None):
         title = self.get_argument("title")
         text = self.get_argument("text")
         if id:
-            blog = Blog.select().where(Blog.id == id).get()
+            blog = await self.application.objects.get(Blog, id=id)
             blog.title = title
             blog.text = text
-            blog.save()
-            return self.redirect(self.reverse_url("blog_detail", blog.id))
-        blog = Blog.create(title=title, text=text)
+            blog = await self.application.objects.update(blog)
+            return self.redirect(self.reverse_url("blog_detail", id))
+        blog = await self.application.objects.create(Blog, title=title, text=text)
         self.redirect(self.reverse_url("blog_detail", blog.id))
 
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-    http_server = tornado.httpserver.HTTPServer(Application())
-    http_server.listen(options.port)
+
+    handlers = [
+        url(r"/", BlogList, name="blog_list"),
+        url(r"/blog-detail/([0-9Xx\-]+)/", BlogDetail, name="blog_detail"),
+        url(r"/blog-create/", BlogCreate, name="blog_create"),
+        url(r"/blog-edit/([0-9Xx\-]+)/", BlogCreate, name="blog_edit"),
+    ]
+
+    settings = dict(
+        debug=True, template_path=os.path.join(os.path.dirname(__file__), "templates"),
+    )
+
+    app = tornado.web.Application(handlers, **settings)
+    app.listen(options.port)
+
+    # allowing Peewee to perform only async operation
+    db.set_allow_sync(False)
+    app.objects = Manager(db)
+
     tornado.ioloop.IOLoop.current().start()
--- models.py
+++ models.py
@@ -1,7 +1,10 @@
+import asyncio
 import datetime
-from peewee import *
 
-db = PostgresqlDatabase(
+import peewee_async
+from peewee import Model, CharField, TextField, DateTimeField
+
+db = peewee_async.PostgresqlDatabase(
     "blog_tornado", user="blog_tornado", password="blog_tornado", host="127.0.0.1",
 )
 
@@ -13,9 +16,58 @@ class Blog(Model):
     published_date = DateTimeField(null=True)
 
     class Meta:
-        database = db 
+        database = db
+
+    def __str__(self):
+        return self.title
 
 
-db.connect()
+##########################################################################################
+###############                     Sync Peeweee
+##########################################################################################
+# Sync code
+# db.connect()
+# db.create_tables([Blog])
 
-db.create_tables([Blog])
+# Blog.create_table(True)
+# Blog.create(title="Yo, I can do it sync!",text="Yo, I can do it sync!")
+# db.close()
+
+
+##########################################################################################
+###############                     Async Peeweee
+##########################################################################################
+# loop = asyncio.new_event_loop()
+
+# # Create async models manager:
+# objects = peewee_async.Manager(db, loop=loop)
+
+# # this code is sync, because for creating/dropping table its good to perform sync operation
+# Blog.create_table(True)
+
+
+# objects.database.allow_sync = False  # this will raise AssertionError on ANY sync call
+
+
+# async def create_or_get_blog():
+#     # Add new Blog
+#     await objects.create_or_get(
+#         Blog, title="title", text="Peewee is AWESOME with async!"
+#     )
+
+#     # Get one by title
+#     blog = await objects.get(Blog, title="title")
+#     print("Was:", blog.text)
+
+#     # Save with new text
+#     blog.text = "Peewee is SUPER awesome with async!"
+#     await objects.update(blog)
+#     print("New:", blog.text)
+
+
+# loop.run_until_complete(create_or_get_blog())
+# loop.close()
+
+# # Clean up, can do it sync again:
+# with objects.allow_sync():
+#     Blog.drop_table(True)
